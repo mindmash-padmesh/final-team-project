5390de7a276d3c01062eeb14406195d6b2bb5f49
rajnishmishra-lgtm <rajnish.mishra@mindmashlabs.com>
2026-07-25 00:24:50 +0530

    Added Layout of the page

--- Employee-Management-Portal/src/Routes/AppRoutes.jsx
+++ Employee-Management-Portal/src/Routes/AppRoutes.jsx
@@ -1,1 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
+import Layout from '../Layout/Layout';
+
+function AppRoutes(){
+    return(
+        <>
+        <Layout/>
+        </>
+    )
+}
+
+export default AppRoutes;
