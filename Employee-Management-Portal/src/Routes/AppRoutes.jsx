cc58758a75066815223959b996f57f4d45aa341d
Padmesh Kumar Singh <singhpadmeh90@gmail.com>
2026-07-29 15:58:50 +0530

    Refactor Employees page using Material UI DataGrid

--- Employee-Management-Portal/src/Routes/AppRoutes.jsx
+++ Employee-Management-Portal/src/Routes/AppRoutes.jsx
@@ -1,12 +1,22 @@
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route,Navigate} from 'react-router-dom';
 import Layout from '../Layout/Layout';
-
+import Employees from "../Pages/Employees";
+import AddEmployee from "../Pages/AddEmployee";
+import EmployeeDetails from "../Pages/EmployeeDetails";
+import EditEmployee from "../Pages/EditEmployee";
 function AppRoutes(){
-    return(
-        <>
-        <Layout/>
-        </>
-    )
+  return (
+    < Routes>
+      <Route path="/" element={<Navigate to="/employees" replace />} />
+      
+      <Route element={<Layout />}>
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/employees/:id" element={<EmployeeDetails />} />
+        <Route path="/employees/edit/:id" element={<EditEmployee/>}/>
+      </Route>
+    </Routes>
+  );  
 }
 
 export default AppRoutes;
