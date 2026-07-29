4e1cfd00dd2ea75019a1484630acdf1a5b918085
rajnishmishra-lgtm <rajnish.mishra@mindmashlabs.com>
2026-07-29 17:17:12 +0530

    Attendance

--- Employee-Management-Portal/src/Routes/AppRoutes.jsx
+++ Employee-Management-Portal/src/Routes/AppRoutes.jsx
@@ -6,14 +6,22 @@ import Login from "../Pages/Login";
 import Dashboard from "../Pages/Dashboard";
 import NotFound from "../Pages/NotFound";
 import Timesheets from "../Pages/Timesheets";
+import Home from "../Pages/Home";
+import Profile from "../Pages/Profile";
+import Leave from "../Pages/Leave";
+import Attendance from "../Pages/Attendance";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout />}>
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="attendance" element={<Attendance/>}/>
+        <Route path="leaves" element={<Leave/>}/>
         <Route path="timesheets" element={<Timesheets/>}/>
+        <Route path="profile" element={<Profile/>}/>
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
