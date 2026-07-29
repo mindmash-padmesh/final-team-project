0cb281f6169709caed4168985dbbaf5f7d7f0ffd
rajnishmishra-lgtm <rajnish.mishra@mindmashlabs.com>
2026-07-29 17:35:47 +0530

    Fixed Routing

--- Employee-Management-Portal/src/Routes/AppRoutes.jsx
+++ Employee-Management-Portal/src/Routes/AppRoutes.jsx
@@ -10,6 +10,10 @@ import Home from "../Pages/Home";
 import Profile from "../Pages/Profile";
 import Leave from "../Pages/Leave";
 import Attendance from "../Pages/Attendance";
+import Employees from "../Pages/Employees";
+import AddEmployee from "../Pages/AddEmployee";
+import EmployeeDetails from "../Pages/EmployeeDetails";
+import EditEmployee from "../Pages/EditEmployee";
 
 function AppRoutes() {
   return (
@@ -18,11 +22,16 @@ function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout />}>
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/employees/:id" element={<EmployeeDetails />} />
+        <Route path="/edit-employee/:id" element={<EditEmployee />} />
         <Route path="attendance" element={<Attendance/>}/>
         <Route path="leaves" element={<Leave/>}/>
         <Route path="timesheets" element={<Timesheets/>}/>
         <Route path="profile" element={<Profile/>}/>
       </Route>
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   );
 }
